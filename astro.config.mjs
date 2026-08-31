599f02c1b83ca4f13aac461ce815f5a15f9c7d3d
koconnor32 <ocon0444@umn.edu>
2026-08-31 09:37:47 -0500

    Fix site/base config to match mn-lowland-conifers/05-webpage repo
    Was still pointing at placeholder koconnor32.github.io/peatland-carbon-mapping values.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://koconnor32.github.io',
-	base: '/peatland-carbon-mapping',
+	site: 'https://mn-lowland-conifers.github.io',
+	base: '/05-webpage',
 });
